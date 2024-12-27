f91da0baa4686954abadc855741a83e993cfba50
hseun <hseun0732@dms.hs.kr>
2024-12-27 15:32:48 +0900

    💡 :: font

--- app/src/main/java/com/loopcat/helper/ui/theme/Type.kt
+++ app/src/main/java/com/loopcat/helper/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.loopcat.helper.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.loopcat.helper.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,25 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val PretendardFamily = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.pretendard_regular,
+            weight = FontWeight.Normal
+        ),
+        Font(
+            resId = R.font.pretendard_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.pretendard_semibold,
+            weight = FontWeight.SemiBold
+        ),
+        Font(
+            resId = R.font.pretendard_bold,
+            weight = FontWeight.Bold
+        )
+    )
 )
