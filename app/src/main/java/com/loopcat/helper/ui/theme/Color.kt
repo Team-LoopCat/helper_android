80d012815d5846d0e6fda97a53691e735a419d66
hseun <hseun0732@dms.hs.kr>
2024-12-27 15:32:56 +0900

    💡 :: color

--- app/src/main/java/com/loopcat/helper/ui/theme/Color.kt
+++ app/src/main/java/com/loopcat/helper/ui/theme/Color.kt
@@ -2,10 +2,48 @@ package com.loopcat.helper.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+sealed class HelperColor(
+    val main: Color,
+    val main50: Color,
+    val main100: Color,
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    val gray000: Color,
+    val gray100: Color,
+    val gray200: Color,
+    val gray300: Color,
+    val gray400: Color,
+    val gray500: Color,
+    val gray600: Color,
+    val gray700: Color,
+    val gray800: Color,
+    val gray900: Color,
+
+    val red: Color,
+    val blue: Color,
+    val dialogBlue: Color,
+    val black: Color,
+    val white: Color
+) {
+    data object Light: HelperColor(
+        main = Color(0xFFFF9900),
+        main50 = Color(0xFFFFE2B7),
+        main100 = Color(0xFFFFBD5A),
+
+        gray000 = Color(0xFFFDFDFD),
+        gray100 = Color(0xFFEBEBEB),
+        gray200 = Color(0xFFD3D3D3),
+        gray300 = Color(0xFFBEBEBE),
+        gray400 = Color(0xFFA7A7A7),
+        gray500 = Color(0xFF929292),
+        gray600 = Color(0xFF7C7C7C),
+        gray700 = Color(0xFF505050),
+        gray800 = Color(0xFF343434),
+        gray900 = Color(0xFF121212),
+
+        red = Color(0xFFEA2E33),
+        blue = Color(0xFF2E41EA),
+        dialogBlue = Color(0xFF5EB3F7),
+        black = Color(0xFF000000),
+        white = Color(0xFFFFFFFF)
+    )
+}
--- app/src/main/java/com/loopcat/helper/ui/theme/Theme.kt
+++ app/src/main/java/com/loopcat/helper/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = HelperColor.Light.main,
+    secondary = HelperColor.Light.main100,
+    tertiary = HelperColor.Light.main50
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = HelperColor.Light.main,
+    secondary = HelperColor.Light.main100,
+    tertiary = HelperColor.Light.main50
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,17 +37,12 @@ private val LightColorScheme = lightColorScheme(
 fun HelperTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if(darkTheme) {
+        darkColorScheme()
+    } else {
+        lightColorScheme()
     }
 
     MaterialTheme(
