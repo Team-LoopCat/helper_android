92835797117cb515f6e3d7117d8f2c1687b0726a
hseun <hseun0732@dms.hs.kr>
2024-12-31 15:38:12 +0900

    💄 :: password input

--- app/src/main/java/com/loopcat/helper/ui/Components.kt
+++ app/src/main/java/com/loopcat/helper/ui/Components.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,11 +24,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.loopcat.helper.R
 import com.loopcat.helper.ui.theme.Black
 import com.loopcat.helper.ui.theme.Gray300
 import com.loopcat.helper.ui.theme.Gray400
@@ -145,5 +153,90 @@ fun HelperInput(
                 innerTextField()
             }
         )
+    }
+}
+
+@Composable
+fun HelperPasswordInput(
+    modifier: Modifier = Modifier,
+    input: String,
+    hint: String,
+    onValueChange: (String) -> Unit
+) {
+    var isShowPassword by remember { mutableStateOf(false) }
+    var isFocused by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier
+            .padding(
+                start = 30.dp,
+                end = 30.dp
+            )
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(
+                color = White,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = if (isFocused) Main else Gray300,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(
+                start = 18.dp,
+                end = 18.dp,
+                top = 14.dp,
+                bottom = 14.dp
+            )
+            .onFocusChanged { focusState ->
+                isFocused = focusState.isFocused
+            }
+    ) {
+        BasicTextField(
+            modifier = modifier
+                .align(Alignment.CenterStart)
+                .padding(
+                    end = 24.dp
+                )
+                .fillMaxWidth(),
+            value = input,
+            onValueChange = onValueChange,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            visualTransformation = if (isShowPassword) VisualTransformation.None else PasswordVisualTransformation(),
+            cursorBrush = SolidColor(Main),
+            textStyle = TextStyle(
+                color = Black,
+                fontFamily = Pretendard,
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp
+            ),
+            decorationBox = { innerTextField ->
+                if(input.isEmpty()) {
+                    InputPlaceHolder(hint = hint)
+                }
+                innerTextField()
+            }
+        )
+        IconButton(
+            modifier = modifier
+                .align(Alignment.CenterEnd),
+            onClick = {
+                isShowPassword = !isShowPassword
+            }
+        ) {
+            val passwordIcon = if(isShowPassword) R.drawable.icon_showpassword else R.drawable.icon_password
+            val contentDescription = if(isShowPassword) "Hide Password Icon" else "Show Password Icon"
+
+            Icon(
+                modifier = modifier.size(20.dp),
+                painter = painterResource(id = passwordIcon),
+                contentDescription = contentDescription
+            )
+        }
     }
 }
