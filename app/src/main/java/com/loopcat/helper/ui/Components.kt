34d7a3a88bc106d7998e1edaeb31b376f9ecf2dc
hseun <hseun0732@dms.hs.kr>
2024-12-31 16:06:02 +0900

    💄 :: TopAppBar

--- app/src/main/java/com/loopcat/helper/ui/Components.kt
+++ app/src/main/java/com/loopcat/helper/ui/Components.kt
@@ -13,9 +13,13 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -34,14 +39,57 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.loopcat.helper.R
 import com.loopcat.helper.ui.theme.Black
 import com.loopcat.helper.ui.theme.Gray300
 import com.loopcat.helper.ui.theme.Gray400
+import com.loopcat.helper.ui.theme.Gray700
 import com.loopcat.helper.ui.theme.Main
 import com.loopcat.helper.ui.theme.Pretendard
 import com.loopcat.helper.ui.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelperTopBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    isBack: Boolean = false,
+    onClick: () -> Unit = {}
+) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    CenterAlignedTopAppBar(
+        modifier = modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        title = {
+            Text(
+                text = title,
+                style = TextStyle(
+                    fontFamily = Pretendard,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Gray700
+                ),
+                letterSpacing = 0.2.em
+            )
+        },
+        navigationIcon = {
+            if (isBack) {
+                IconButton(
+                    onClick = onClick
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon_topbar_back),
+                        contentDescription = "TopBar Back Button"
+                    )
+                }
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
 
 @Composable
 fun HelperButton(
