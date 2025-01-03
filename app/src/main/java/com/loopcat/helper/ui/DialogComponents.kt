7be1d565c2d5e2e3ab6922081996b71830e5a805
hseun <hseun0732@dms.hs.kr>
2025-01-03 18:38:53 +0900

    👀 :: delete import

--- app/src/main/java/com/loopcat/helper/ui/DialogComponents.kt
+++ app/src/main/java/com/loopcat/helper/ui/DialogComponents.kt
@@ -12,16 +12,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
