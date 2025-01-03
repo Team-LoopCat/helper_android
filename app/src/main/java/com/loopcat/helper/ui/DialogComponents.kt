443bf3448a8654900d744ab8a313ad90ec1d7ccd
hseun <hseun0732@dms.hs.kr>
2025-01-03 18:37:47 +0900

    💄 :: ConfirmDialog

--- app/src/main/java/com/loopcat/helper/ui/DialogComponents.kt
+++ app/src/main/java/com/loopcat/helper/ui/DialogComponents.kt
@@ -3,7 +3,10 @@ package com.loopcat.helper.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,7 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -33,8 +36,74 @@ import com.loopcat.helper.R
 import com.loopcat.helper.ui.theme.Black
 import com.loopcat.helper.ui.theme.DialogBlue
 import com.loopcat.helper.ui.theme.Gray400
+import com.loopcat.helper.ui.theme.Gray700
 import com.loopcat.helper.ui.theme.Pretendard
+import com.loopcat.helper.ui.theme.Red
 import com.loopcat.helper.ui.theme.White
+
+@Composable
+fun HelperConfirmClick(
+    modifier: Modifier = Modifier,
+    onClickConfirm: () -> Unit,
+    onClickCancel: () -> Unit
+) {
+    HorizontalDivider(
+        thickness = 0.4.dp,
+        color = Gray400
+    )
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+            .padding(0.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.dialog_cancel),
+            style = TextStyle(
+                fontFamily = Pretendard,
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp,
+                color = Red
+            ),
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .weight(1f)
+                .clickable {
+                    onClickCancel()
+                }
+                .padding(
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+        )
+        VerticalDivider(
+            thickness = 0.4.dp,
+            color = Gray400,
+            modifier = modifier
+                .fillMaxHeight()
+                .align(Alignment.CenterVertically)
+        )
+        Text(
+            text = stringResource(id = R.string.dialog_okay),
+            style = TextStyle(
+                fontFamily = Pretendard,
+                fontWeight = FontWeight.Medium,
+                fontSize = 16.sp,
+                color = DialogBlue
+            ),
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .weight(1f)
+                .clickable {
+                    onClickConfirm()
+                }
+                .padding(
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+        )
+    }
+}
 
 @Composable
 fun HelperAlertDialog(
@@ -95,6 +164,68 @@ fun HelperAlertDialog(
                             top = 12.dp,
                             bottom = 12.dp
                         )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HelperConfirmDialog(
+    modifier: Modifier = Modifier,
+    title: String,
+    onClickCancel: () -> Unit,
+    onClickConfirm: () -> Unit,
+    isDelete: Boolean = false,
+    description: String = ""
+) {
+    Dialog(
+        onDismissRequest = onClickCancel,
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true
+        )
+    ) {
+        Card(
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(
+                        color = White
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = modifier.height(42.dp))
+                Text(
+                    text = title,
+                    style = TextStyle(
+                        fontFamily = Pretendard,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 18.sp,
+                        color = Black
+                    ),
+                    textAlign = TextAlign.Center
+                )
+                if (isDelete) {
+                    Spacer(modifier = modifier.height(12.dp))
+                    Text(
+                        text = description,
+                        style = TextStyle(
+                            fontFamily = Pretendard,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 12.sp,
+                            color = Gray700
+                        ),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Spacer(modifier = modifier.height(40.dp))
+                HelperConfirmClick(
+                    onClickConfirm = onClickConfirm,
+                    onClickCancel = onClickCancel
                 )
             }
         }
