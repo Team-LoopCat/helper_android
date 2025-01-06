99db9a40cda62f45ad255305662dc217b9904946
hseun <hseun0732@dms.hs.kr>
2025-01-06 16:21:08 +0900

    ✨ :: error 처리

--- app/src/main/java/com/loopcat/helper/splash/SplashScreen.kt
+++ app/src/main/java/com/loopcat/helper/splash/SplashScreen.kt
@@ -11,23 +11,31 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.loopcat.helper.MainActivity
 import com.loopcat.helper.R
+import com.loopcat.helper.ui.HelperAlertDialog
 import com.loopcat.helper.ui.theme.White
 import kotlinx.coroutines.delay
+import kotlin.jvm.Throws
 
 @Composable
 fun HelperSplashScreen(modifier: Modifier = Modifier) {
     val alpha = remember { Animatable(0f) }
     val context = LocalContext.current
+
+    var isException by remember { mutableStateOf(false) }
     
     LaunchedEffect(key1 = Unit) {
         alpha.animateTo(
@@ -36,10 +44,15 @@ fun HelperSplashScreen(modifier: Modifier = Modifier) {
         )
         delay(1000L)
 
-        Intent(context, MainActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }.let { intent ->
-            ContextCompat.startActivity(context, intent, null)
+        try {
+            Intent(context, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }.let { intent ->
+                ContextCompat.startActivity(context, intent, null)
+            }
+        } catch (e: Exception) {
+            println(e.message)
+            isException = true
         }
     }
 
@@ -57,5 +70,13 @@ fun HelperSplashScreen(modifier: Modifier = Modifier) {
                 .fillMaxWidth(),
             contentScale = ContentScale.FillWidth
         )
+        if (isException) {
+            HelperAlertDialog(
+                title = stringResource(id = R.string.splash_exception),
+                onClick = {
+                    android.os.Process.killProcess(android.os.Process.myPid())
+                }
+            )
+        }
     }
 }
