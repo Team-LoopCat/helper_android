6517f6ac9182e4c0a0fb120ac3344e9131a2fba4
hseun <hseun0732@dms.hs.kr>
2025-01-09 15:27:46 +0900

    👀 :: 변수 분리

--- app/src/main/java/com/loopcat/helper/ui/DropdownComponent.kt
+++ app/src/main/java/com/loopcat/helper/ui/DropdownComponent.kt
@@ -10,10 +10,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -39,6 +36,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
@@ -51,7 +49,10 @@ import com.loopcat.helper.ui.theme.Main
 import com.loopcat.helper.ui.theme.Pretendard
 import com.loopcat.helper.ui.theme.White
 
-private const val ANIMATION_DURATION = 300
+private val dropdownAnimationSpec = tween<IntSize>(
+    durationMillis = 300,
+    easing = FastOutSlowInEasing
+)
 
 @Composable
 fun LargeDropdownHeader(
@@ -172,10 +173,7 @@ fun LargeDropdownMenuContent(
                 shape = RoundedCornerShape(8.dp)
             )
             .animateContentSize(
-                animationSpec = tween(
-                    durationMillis = ANIMATION_DURATION,
-                    easing = FastOutSlowInEasing
-                )
+                 animationSpec = dropdownAnimationSpec
             )
     ) {
         Column {
@@ -386,10 +384,7 @@ fun SmallDropdownMenuContent(
                 shape = RoundedCornerShape(8.dp)
             )
             .animateContentSize(
-                animationSpec = tween(
-                    durationMillis = ANIMATION_DURATION,
-                    easing = FastOutSlowInEasing
-                )
+                animationSpec = dropdownAnimationSpec
             )
     ) {
         Column {
