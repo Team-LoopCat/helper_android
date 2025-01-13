18f7f8fa2a14061ab51bc5ce3015edf6023c81b5
hseun <hseun0732@dms.hs.kr>
2025-01-13 15:43:08 +0900

    🌱 :: signup regex

---
+++ app/src/main/java/com/loopcat/helper/ui/auth/AuthRegex.kt
@@ -0,0 +1,18 @@
+package com.loopcat.helper.ui.auth
+
+enum class AuthRegexType {
+    ID,
+    PASSWORD,
+    MAIL,
+    NICKNAME
+}
+
+fun checkRegex(type: AuthRegexType, input: String): Boolean {
+    val regex = when(type) {
+        AuthRegexType.ID -> "^[a-zA-Z0-9_]{5,20}"
+        AuthRegexType.PASSWORD -> "^[a-zA-Z0-9!#$%^&*+=_-]{8,20}"
+        AuthRegexType.MAIL -> "^[a-zA-Z0-9] + @dms.hs.kr$"
+        AuthRegexType.NICKNAME -> "^[a-zA-Z0-9가-힣!#\$%^&*/'\\\";:_=+-]{2,10}"
+    }.toRegex()
+    return regex.matches(input)
+}
