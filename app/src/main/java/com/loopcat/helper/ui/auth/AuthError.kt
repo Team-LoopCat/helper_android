3faa858da82089fb5e7349b287d760f9473723fd
hseun <hseun0732@dms.hs.kr>
2025-01-15 16:59:59 +0900

    🌱 :: mailError

--- app/src/main/java/com/loopcat/helper/ui/auth/AuthError.kt
+++ app/src/main/java/com/loopcat/helper/ui/auth/AuthError.kt
@@ -8,6 +8,12 @@ enum class AuthErrorType {
     NOT_SAME_PW,
     OVERLAP_ID,
 
+    MAIL_REGEX,
+    OVERLAP_MAIL,
+    MAIL_MODIFY,
+    WRONG_CODE,
+    CODE_TIMEOUT,
+
     WRONG_ID_OR_PW,
     NONE
 }
@@ -18,6 +24,12 @@ fun authErrorMessage(error: AuthErrorType): Int {
         AuthErrorType.PW_REGEX -> R.string.error_pw_regex
         AuthErrorType.NOT_SAME_PW -> R.string.error_not_same_pw
         AuthErrorType.OVERLAP_ID -> R.string.error_overlap_id
+
+        AuthErrorType.MAIL_REGEX -> R.string.error_mail_regex
+        AuthErrorType.OVERLAP_MAIL -> R.string.error_overlap_mail
+        AuthErrorType.MAIL_MODIFY -> R.string.error_mail_modify
+        AuthErrorType.WRONG_CODE -> R.string.error_wrong_code
+        AuthErrorType.CODE_TIMEOUT -> R.string.error_timeout
 
         AuthErrorType.WRONG_ID_OR_PW -> R.string.error_login
         AuthErrorType.NONE -> R.string.error_none
