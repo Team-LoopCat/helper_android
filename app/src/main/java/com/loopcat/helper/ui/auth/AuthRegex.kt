5f6b65f5dbbb5d895602aea7f9ebc7488730c557
hseun <hseun0732@dms.hs.kr>
2025-01-15 17:00:25 +0900

    🐛 :: mail regex

--- app/src/main/java/com/loopcat/helper/ui/auth/AuthRegex.kt
+++ app/src/main/java/com/loopcat/helper/ui/auth/AuthRegex.kt
@@ -11,7 +11,7 @@ fun checkRegex(type: AuthRegexType, input: String): Boolean {
     val regex = when(type) {
         AuthRegexType.ID -> "^[a-zA-Z0-9_]{5,20}"
         AuthRegexType.PASSWORD -> "^[a-zA-Z0-9!#$%^&*+=_-]{8,20}"
-        AuthRegexType.MAIL -> "^[a-zA-Z0-9] + @dms.hs.kr$"
+        AuthRegexType.MAIL -> "^[a-zA-Z0-9._%+-]+@dsm\\.hs\\.kr$"
         AuthRegexType.NICKNAME -> "^[a-zA-Z0-9가-힣!#\$%^&*/'\\\";:_=+-]{2,10}"
     }.toRegex()
     return regex.matches(input)
