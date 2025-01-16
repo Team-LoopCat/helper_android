d82ceb420f25a6cdde3c8b028eec97c87875dc5b
hseun <hseun0732@dms.hs.kr>
2025-01-16 16:33:44 +0900

    🌱 :: AuthComponents

--- app/src/main/java/com/loopcat/helper/ui/auth/AuthComponents.kt
+++ app/src/main/java/com/loopcat/helper/ui/auth/AuthComponents.kt
@@ -1,17 +1,23 @@
 package com.loopcat.helper.ui.auth
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -25,28 +31,32 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.loopcat.helper.R
 import com.loopcat.helper.ui.InputPlaceHolder
 import com.loopcat.helper.ui.theme.Black
 import com.loopcat.helper.ui.theme.Gray300
+import com.loopcat.helper.ui.theme.Gray500
 import com.loopcat.helper.ui.theme.Main
 import com.loopcat.helper.ui.theme.Main100
 import com.loopcat.helper.ui.theme.Pretendard
 import com.loopcat.helper.ui.theme.Red
 import com.loopcat.helper.ui.theme.White
+import com.loopcat.helper.ui.utills.noRippleClickable
 import com.loopcat.helper.ui.utills.singleClickEvent
 
 @Composable
 fun AuthTitle(
-    modifier: Modifier = Modifier,
     text: String
 ) {
     Text(
@@ -220,6 +230,60 @@ fun SmallInputWithButton(
             enable = enable,
             buttonText = buttonText,
             onClick = onClick
+        )
+    }
+}
+
+@Composable
+fun ProfileModify(
+    modifier: Modifier = Modifier,
+    imageUrl: Uri,
+    onImageSelected: (Uri?) -> Unit
+) {
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        onImageSelected(uri)
+    }
+    Box(
+        modifier = modifier
+            .size(120.dp)
+            .noRippleClickable {
+                launcher.launch("image/*")
+            }
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = "Profile Image",
+            modifier = modifier
+                .fillMaxSize()
+                .align(Alignment.Center)
+                .clip(CircleShape)
+                .border(
+                    width = 1.dp,
+                    color = Gray500,
+                    shape = CircleShape
+                )
+        )
+        Image(
+            painter = painterResource(id = R.drawable.icon_pencil),
+            contentDescription = "Profile Modify",
+            modifier = modifier
+                .padding(
+                    end = 8.dp
+                )
+                .align(Alignment.BottomEnd)
+                .size(28.dp)
+                .background(
+                    color = White,
+                    shape = CircleShape
+                )
+                .border(
+                    width = 1.dp,
+                    color = Gray500,
+                    shape = CircleShape
+                )
+                .padding(7.dp)
         )
     }
 }
