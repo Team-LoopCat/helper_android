379a3452054448a6528917bd50129dd044beed9b
hseun <hseun0732@dms.hs.kr>
2025-01-17 16:10:29 +0900

    👀 :: nickname regex

--- app/src/main/java/com/loopcat/helper/ui/auth/AuthRegex.kt
+++ app/src/main/java/com/loopcat/helper/ui/auth/AuthRegex.kt
@@ -12,7 +12,7 @@ fun checkRegex(type: AuthRegexType, input: String): Boolean {
         AuthRegexType.ID -> "^[a-zA-Z0-9_\\.-]{5,20}$"
         AuthRegexType.PASSWORD -> "^[a-zA-Z0-9~!@#$%^&*()_+=?\\.-]{8,20}$"
         AuthRegexType.MAIL -> "^[a-zA-Z0-9._%+-]+@dsm\\.hs\\.kr$"
-        AuthRegexType.NICKNAME -> "^[a-zA-Z0-9가-힣_\\.-]{2,10}$"
+        AuthRegexType.NICKNAME -> "^[a-zA-Z0-9ㄱ-ㅎ가-힣_\\.-]{2,10}$"
     }.toRegex()
     return regex.matches(input)
 }
