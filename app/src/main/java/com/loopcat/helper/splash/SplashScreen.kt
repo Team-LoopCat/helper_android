f295ad89a8b29e14d542d09cc326a1b53e79cac8
hseun <hseun0732@dms.hs.kr>
2025-04-13 22:44:32 +0900

    ✨ :: splash

--- app/src/main/java/com/loopcat/helper/splash/SplashScreen.kt
+++ app/src/main/java/com/loopcat/helper/splash/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.loopcat.helper.splash
 
+import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
@@ -16,11 +17,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.loopcat.helper.R
+import com.loopcat.helper.splash.viewmodel.SplashViewModel
 import com.loopcat.helper.ui.HelperAlertDialog
 import com.loopcat.helper.ui.theme.White
 import kotlinx.coroutines.delay
@@ -30,22 +34,30 @@ const val NAVIGATION_SPLASH = "splash"
 @Composable
 fun HelperSplashScreen(
     modifier: Modifier = Modifier,
-    navToLogin: () -> Unit
+    splashViewModel: SplashViewModel = viewModel(),
+    navToLogin: () -> Unit,
+    navToMain: () -> Unit
 ) {
     val alpha = remember { Animatable(0f) }
     var isException by remember { mutableStateOf(false) }
-    
-    LaunchedEffect(key1 = Unit) {
+    val isLoggedIn by splashViewModel.isLoggedIn
+
+    LaunchedEffect(Unit) {
         alpha.animateTo(
             targetValue = 1f,
             animationSpec = tween(500)
         )
         delay(1000L)
-
         try {
-            navToLogin()
+            when (isLoggedIn) {
+                true -> navToMain()
+                false -> navToLogin()
+                else -> {
+                    android.os.Process.killProcess(android.os.Process.myPid())
+                }
+            }
         } catch (e: Exception) {
-            println(e.message)
+            Log.e("splash screen", e.toString())
             isException = true
         }
     }
@@ -54,6 +66,7 @@ fun HelperSplashScreen(
         modifier = modifier
             .fillMaxSize()
             .background(White)
+            .alpha(alpha.value)
     ) {
         Image(
             painter = painterResource(id = R.drawable.logo_main),
